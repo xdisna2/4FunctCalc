fdbb1697c395f96b6ee084c25749c403a32826a2
xdisna2 <aznthonyle45@gmail.com>
2020-05-20 12:45:11 -0700

    Tkinter file to experiment

--- tkinter_experiment.py
+++ tkinter_experiment.py
@@ -1,8 +1,21 @@
 from tkinter import *
-# Themed Widgets
 from tkinter import ttk
 
 # Creates top level window
 root = Tk()
+
+# Creates a new button widget
 button = ttk.Button(root, text='Click Me')
+
+# Always pack the button
 button.pack()
+
+# Changing text
+button['text'] = 'Hi There'
+ttk.Label(root, text='Hello World!').pack()
+
+# Always declare mainloop to keep it open
+root.mainloop()
+
+
+
