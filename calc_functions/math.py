d65265b7c023ab3731f04e9402fdb318205e96b8
xdisna2 <aznthonyle45@gmail.com>
2020-05-20 09:58:20 -0700

    Basic coding for calculator done (assuming integer values)

--- calc_functions/math.py
+++ calc_functions/math.py
@@ -9,7 +9,7 @@ def sub(num1, num2):
 
 
 # Divide
-def div(num1,num2):
+def div(num1, num2):
     return num1 / num2
 
 
--- main.py
+++ main.py
@@ -1,4 +1,37 @@
+from calc_functions import math
 
 # 4 Function Calculator
 if __name__ == '__main__':
-    print()
+
+    # Calculator will loop forever until prompted
+    again = 'y'
+    while again == 'y':
+        # Asks user to input 2 numbers (for now...)
+        num1 = int(input())
+        num2 = int(input())
+
+        # Asks user to do "what" to the numbers (for now...)
+        print("Please select an operation")
+        functions = ['Addition', 'Subtraction', 'Division', 'Multiplication']
+        for i in range (4):
+            print(f"{i+1}: {functions[i]}")
+
+        user_input = int(input())
+        # Will compute results based on number selection
+        if user_input == 1:
+            result = math.add(num1, num2)
+
+        elif user_input == 2:
+            result = math.sub(num1, num2)
+
+        elif user_input == 3:
+            result = math.div(num1, num2)
+
+        else:
+            result = math.multi(num1, num2)
+
+        print(result)
+
+        again = input("Enter 'y' to continue")
+
+    print("Goodbye")
