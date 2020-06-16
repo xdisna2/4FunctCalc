3556c31b11807026033af34b8804d7891905c2da
Anthony Le <aznthonyle45@gmail.com>
2020-06-15 19:00:55 -0700

    Added button numbers 0-9
    Signed-off-by: Anthony Le <aznthonyle45@gmail.com>

--- calc_gui.py
+++ calc_gui.py
@@ -10,7 +10,17 @@ numbers.pack()
 
 numbers.config(height = 150, width = 250)
 numbers.config(relief = RIDGE)
+num0 = ttk.Button(numbers, text = '0').grid()
 num1 = ttk.Button(numbers, text = '1').grid()
+num2 = ttk.Button(numbers, text = '2').grid()
+num3 = ttk.Button(numbers, text = '3').grid()
+num4 = ttk.Button(numbers, text = '4').grid()
+num5 = ttk.Button(numbers, text = '5').grid()
+num6 = ttk.Button(numbers, text = '6').grid()
+num7 = ttk.Button(numbers, text = '7').grid()
+num8 = ttk.Button(numbers, text = '8').grid()
+num9 = ttk.Button(numbers, text = '9').grid()
+
 
 
 root.mainloop()
