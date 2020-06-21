38322572fdeaae0240e2a31041ee5a29235274b2
Anthony Le <aznthonyle45@gmail.com>
2020-06-21 11:05:40 -0700

    Added command callbacks to output numbers, still need to configure the clear button.
    Signed-off-by: Anthony Le <aznthonyle45@gmail.com>

--- calc_gui.py
+++ calc_gui.py
@@ -1,5 +1,14 @@
 from tkinter import *
 from tkinter import ttk
+user = ''
+
+def number_disp(number):
+    global user
+    user += str(number)
+    output.config(text=user)
+
+def clear(num):
+    output.config(text=str(num))
 
 # Create the top-level window
 root = Tk()
@@ -21,17 +30,17 @@ numbers.grid(row = 0, column = 0)
 
 # Configures the frame height and width
 numbers.config(height = 150, width = 200, relief = RIDGE, padding = 5)
-deci = ttk.Button(numbers, text = '.').grid(row = 3, column = 2)
-num0 = ttk.Button(numbers, text = '0').grid(row = 3, column = 0, columnspan = 2, sticky = W + E)
-num1 = ttk.Button(numbers, text = '1').grid(row = 2, column = 0)
-num2 = ttk.Button(numbers, text = '2').grid(row = 2, column = 1)
-num3 = ttk.Button(numbers, text = '3').grid(row = 2, column = 2)
-num4 = ttk.Button(numbers, text = '4').grid(row = 1, column = 0)
-num5 = ttk.Button(numbers, text = '5').grid(row = 1, column = 1)
-num6 = ttk.Button(numbers, text = '6').grid(row = 1, column = 2)
-num7 = ttk.Button(numbers, text = '7').grid(row = 0, column = 0)
-num8 = ttk.Button(numbers, text = '8').grid(row = 0, column = 1)
-num9 = ttk.Button(numbers, text = '9').grid(row = 0, column = 2)
+deci = ttk.Button(numbers, text = '.', command = lambda : number_disp('.')).grid(row = 3, column = 2)
+num0 = ttk.Button(numbers, text = '0', command = lambda : number_disp(0)).grid(row = 3, column = 0, columnspan = 2, sticky = W + E)
+num1 = ttk.Button(numbers, text = '1', command = lambda : number_disp(1)).grid(row = 2, column = 0)
+num2 = ttk.Button(numbers, text = '2', command = lambda : number_disp(2)).grid(row = 2, column = 1)
+num3 = ttk.Button(numbers, text = '3', command = lambda : number_disp(3)).grid(row = 2, column = 2)
+num4 = ttk.Button(numbers, text = '4', command = lambda : number_disp(4)).grid(row = 1, column = 0)
+num5 = ttk.Button(numbers, text = '5', command = lambda : number_disp(5)).grid(row = 1, column = 1)
+num6 = ttk.Button(numbers, text = '6', command = lambda : number_disp(6)).grid(row = 1, column = 2)
+num7 = ttk.Button(numbers, text = '7', command = lambda : number_disp(7)).grid(row = 0, column = 0)
+num8 = ttk.Button(numbers, text = '8', command = lambda : number_disp(8)).grid(row = 0, column = 1)
+num9 = ttk.Button(numbers, text = '9', command = lambda : number_disp(9)).grid(row = 0, column = 2)
 
 # 4 Function Operations
 op_wd = 8
@@ -57,9 +66,12 @@ sub.config(width = op_wd)
 div.config(width = op_wd)
 multi.config(width = op_wd)
 
-# Equals button
-equate = ttk.Button(calc, text = '=').grid(row = 1, column = 0, columnspan = 2, sticky = W + E)
-
+# Equals button and clear button
+box3 = ttk.Frame(root)
+box3.pack()
+equate = ttk.Button(box3, text = '=').grid(row = 0, column = 0)
+clear = ttk.Button(box3, text = 'C', command=lambda : clear(0)).grid(row = 0, column = 1)
+clear_step = ttk.Button(box3, text = 'CE').grid(row = 0, column = 2)
 
 root.mainloop()
 
--- tkinter_experiment.py
+++ tkinter_experiment.py
@@ -14,10 +14,10 @@ def output(number):
 
 
 # Create buttons 1,2,3 (creating a lambda function makes an anonymous function)
-num1 = ttk.Button(root, text='1', command = lambda: output(1))
-num2 = ttk.Button(root, text='2', command = lambda: output(2))
-num3 = ttk.Button(root, text='3', command = lambda: output(3))
-clear = ttk.Button(root, text = 'Clear', command = lambda: output(0))
+num1 = ttk.Button(root, text='1', command=lambda: output(1))
+num2 = ttk.Button(root, text='2', command=lambda: output(2))
+num3 = ttk.Button(root, text='3', command=lambda: output(3))
+clear = ttk.Button(root, text='Clear', command=lambda: output(0))
 
 # Geometry packs them
 num1.pack()
