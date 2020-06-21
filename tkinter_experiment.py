66811c60a30ff4a888826bd3e0244c699ac8a485
Anthony Le <aznthonyle45@gmail.com>
2020-06-20 19:40:24 -0700

    Shorter Command callback
    Signed-off-by: Anthony Le <aznthonyle45@gmail.com>

--- tkinter_experiment.py
+++ tkinter_experiment.py
@@ -8,28 +8,22 @@ label = ttk.Label(root, text='0')
 label.grid(row = 0, column = 0, columnspan = 3)
 label.pack()
 
-# Functions for output
-def output1():
-    label.config(text='1')
-def output2():
-    label.config(text='2')
-def output3():
-    label.config(text='3')
-def clearall():
-    label.config(text='0')
+# Functions for output (Shorter revised version
+def output(number):
+    label.config(text=number)
 
-# Create buttons 1,2,3
-num1 = ttk.Button(root, text='1', command=output1)
-num2 = ttk.Button(root, text='2', command=output2)
-num3 = ttk.Button(root, text='3', command=output3)
-clear = ttk.Button(root, text='Clear', command=clearall)
+
+# Create buttons 1,2,3 (creating a lambda function makes an anonymous function)
+num1 = ttk.Button(root, text='1', command = lambda: output(1))
+num2 = ttk.Button(root, text='2', command = lambda: output(2))
+num3 = ttk.Button(root, text='3', command = lambda: output(3))
+clear = ttk.Button(root, text = 'Clear', command = lambda: output(0))
 
 # Geometry packs them
 num1.pack()
 num2.pack()
 num3.pack()
 clear.pack()
-
 root.mainloop()
 
 
